82d88b6c73949c17c97899b1421e9954ca781ae6
Gislaine Mello Casa <gislainemcasa@gmail.com>
2023-10-25 07:34:05 -0300

    Add files via upload

---
+++ src/pages/_document.tsx
@@ -0,0 +1,18 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;700;900&display=swap" rel="stylesheet" />
+      <link rel='shortcut icon' href='/favicon.png' type='image/png' />
+      <title>Sujeito</title>
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
